bd9c432f598ed6d4234219517e21752e1719f5d6
tupic98 <00017316@uca.edu.sv>
2020-08-05 17:04:24 -0600

    Updated PostService

--- services/PostService.js
+++ services/PostService.js
@@ -50,4 +50,84 @@ service.create = async ({ title, description, image, user }) => {
   }
 };
 
+service.findOneById = async (_id) => {
+  const serviceResponse = {
+    success: true,
+    content: {
+      message: 'Post found',
+    },
+  };
+
+  try {
+    const post = await PostModel.findById(_id).exec();
+    if (!post) {
+      serviceResponse.success = false;
+      serviceResponse.content = {
+        error: 'Post not found',
+      };
+    } else {
+      serviceResponse.content.post = post;
+    }
+    console.log(serviceResponse);
+    return serviceResponse;
+  } catch (error) {
+    throw new Error('Internal Server Error');
+  }
+};
+
+service.findAll = async (page, limit) => {
+  const serviceResponse = {
+    success: true,
+    content: {},
+  };
+
+  try {
+    const posts = await PostModel.find({}, undefined, {
+      skip: page * limit,
+      limit,
+      sort: [
+        {
+          updatedAt: -1,
+        },
+      ],
+    }).exec();
+    serviceResponse.content = {
+      posts,
+      count: posts.length,
+      page,
+      limit,
+    };
+    return serviceResponse;
+  } catch (error) {
+    throw new Error('Internal Server Error');
+  }
+};
+
+/*
+ * @params post - PostModel
+ */
+service.addLike = async (post) => {
+  let serviceResponse = {
+    success: true,
+    content: {
+      message: 'Post liked',
+    },
+  };
+  try {
+    post.likes += 1;
+    const postUpdated = await post.save();
+    if (!postUpdated) {
+      serviceResponse = {
+        success: false,
+        content: {
+          message: 'Post not liked',
+        },
+      };
+    }
+    return serviceResponse;
+  } catch (error) {
+    throw new Error('Internal Server Error');
+  }
+};
+
 module.exports = service;
