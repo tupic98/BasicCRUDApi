7f98b158ae90fa36655664608c5589c2b8ab617e
tupic98 <00017316@uca.edu.sv>
2020-08-05 17:04:17 -0600

    Updated Post routes

--- routes/api/Post.js
+++ routes/api/Post.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const PostController = require('./../../controllers/api/PostController');
 
 router.post('/', PostController.create);
+router.get('/id/:_id', PostController.findOneById);
+router.get('/all', PostController.findAll);
+router.patch('/like', PostController.addLike);
 
 module.exports = router;
